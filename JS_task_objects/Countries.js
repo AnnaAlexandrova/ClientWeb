f64b2e5cc78b57e0a46f911f1f680c8ba944d8e5
AnnaAlexandrova <alexandrova-anna@bk.ru>
2019-07-19 01:06:50 +0700

    Задача Objects исправления

--- JS_task_objects/Countries.js
+++ JS_task_objects/Countries.js
@@ -1,65 +1,84 @@
-var countries = [{
-    name: "Ireland",
-    cities: [{name: "Dublin", population: 10987234},
-        {name: "Portlaoise", population: 154985}]
-},
-
+var countries = [
+    {
+        name: "Ireland",
+        cities: [
+            {
+                name: "Dublin",
+                population: 10987234
+            },
+            {
+                name: "Portlaoise",
+                population: 154985
+            }
+        ]
+    },
     {
         name: "Russian Federation",
-        cities: [{name: "Moscow", population: 17934123},
-            {name: "Saint-Petersburg", population: 12096234},
-            {name: "Novosibirsk", population: 1576245}]
+        cities: [
+            {
+                name: "Moscow",
+                population: 17934123
+            },
+            {
+                name: "Saint-Petersburg",
+                population: 12096234
+            },
+            {
+                name: "Novosibirsk",
+                population: 1576245
+            }
+        ]
     },
-
     {
         name: "Cuba",
-        cities: [{name: "Havana", population: 7902345},
-            {name: "Trinidad", population: 546238},
-            {name: "Santa Clara", population: 134642}]
+        cities: [
+            {
+                name: "Havana",
+                population: 7902345
+            },
+            {
+                name: "Trinidad",
+                population: 546238
+            },
+            {
+                name: "Santa Clara",
+                population: 134642
+            }
+        ]
     }
 ];
 
-function findMaxCitiesCount(countries) {
-    var maxCitiesCount = 0;
-    for (var country in countries) {
-        if (countries.hasOwnProperty(country)) {
-            if (countries[country].cities.length >= maxCitiesCount) {
-                maxCitiesCount = countries[country].cities.length;
-            }
+function getCountriesWithMaxCitiesCount(countries) {
+    var result = countries.reduce(function (maxCitiesCount, currentCountry) {
+        if (currentCountry.cities.length >= maxCitiesCount) {
+            maxCitiesCount = currentCountry.cities.length;
         }
-    }
+        return maxCitiesCount;
+    }, 0);
 
-    var countriesList = [];
-    for (country in countries) {
-        if (countries.hasOwnProperty(country)) {
-            if (countries[country].cities.length === maxCitiesCount) {
-                countriesList.push(countries[country].name);
-            }
-        }
-    }
-    return countriesList;
+    return countries.filter(function (country) {
+        return country.cities.length === result;
+    }).map(function (country) {
+        return country.name;
+    });
 }
 
-console.log(findMaxCitiesCount(countries));
+console.log(getCountriesWithMaxCitiesCount(countries));
 
 function findCountriesPopulation(countries) {
-    var countriesPopulation = {};
-
-    for (var currentCountry in countries) {
-        if (countries.hasOwnProperty(currentCountry)) {
-            var countryName = countries[currentCountry].name;
-
-            countriesPopulation[countryName] = findPopulation(countries[currentCountry]);
-        }
-    }
-    return countriesPopulation;
+    var countriesList = {};
+    countries.map(function (country) {
+        var countryName = country.name;
+        return countriesList[countryName] = findPopulation(country);
+    });
+    return countriesList;
 }
 
 function findPopulation(country) {
     return country.cities.reduce(function (sum, city) {
         var currentPopulation = city.population;
 
-        return sum + currentPopulation
+        return sum + currentPopulation;
     }, 0);
 }
 
