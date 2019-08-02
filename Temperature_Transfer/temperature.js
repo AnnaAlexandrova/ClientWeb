0cf5e5ad0bf570f1492c041e6a4d77b8e7452ae4
AnnaAlexandrova <alexandrova-anna@bk.ru>
2019-08-02 23:54:08 +0700

    Задача Перевод температур

---
+++ Temperature_Transfer/temperature.js
@@ -0,0 +1,15 @@
+function ready() {
+    document.getElementById("transfer").onclick = function () {
+        var degree = document.getElementById("celsius").value;
+
+        if (isNaN(degree)) {
+            alert("Введите число в шкалу Цельсия");
+        } else {
+            document.getElementById("kelvin").value = +degree + 273.15;
+            document.getElementById("fahrenheit").value = +degree * 1.8 + 32;
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
