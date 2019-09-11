5f75fa84b58d4a9e507102bc1c56574539789101
AnnaAlexandrova <alexandrova-anna@bk.ru>
2019-09-11 13:33:08 +0700

    Задача TodoListVue

---
+++ TodoListVue/TodoList.js
@@ -0,0 +1,72 @@
+Vue.component("todo-list-item", {
+    props: ["item"],
+    data: function () {
+        return {
+            needsEdit: false,
+            previousTodoText: this.item.text
+        };
+    },
+    template: "#todo-list-item-template",
+    methods: {
+        deleteItem: function () {
+            this.$emit("delete-item", this.item);
+        },
+        editItem: function () {
+            this.needsEdit = true;
+        },
+        save: function () {
+            this.$emit("change-item", this.item, this.previousTodoText);
+            this.needsEdit = false;
+        },
+        cancel: function () {
+            this.$emit("cancel-changing", this.item, this.previousTodoText);
+            this.needsEdit = false;
+        }
+    }
+});
+
+new Vue({
+        el: "#app",
+        data: function () {
+            return {
+                items: [],
+                newTodoText: "",
+                isInvalid: false
+            }
+        },
+        methods: {
+            addTodo: function () {
+                if (this.newTodoText.trim() === "") {
+                    this.isInvalid = true;
+                    return;
+                }
+
+                this.isInvalid = false;
+
+                this.items.push({
+                    text: this.newTodoText
+                });
+
+                this.newTodoText = "";
+            },
+
+            changeItem: function (item, previousTodoText) {
+                if (item.text.trim() === "") {
+                    item.text = previousTodoText;
+                } else {
+                    this.previousTodoText = this.item.text;
+                }
+            },
+
+            cancelChanging: function (item, previousTodoText) {
+                item.text = previousTodoText;
+            },
+
+            deleteTodo: function (item) {
+                this.items = this.items.filter(function (x) {
+                    return x !== item;
+                })
+            }
+        }
+    }
+);
